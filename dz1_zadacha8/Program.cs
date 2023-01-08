ea93bde735ac7190b6b342a663a5e6b5ea2af366
sergey titar <titar_seryoga@mail.ru>
2023-01-08 22:10:31 +0700

    описание

---
+++ dz1_zadacha8/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = { 3, 4, -9, 1, 8, 80 };
+int N = array.Length;
+int index = 0;
+
+while (index < N)
+{
+    if (array[index] % 2 == 0)
+        Console.WriteLine(array[index]);
+index++;
+}
