01559e1e9a2bc77942cf1f32f11fd4f599968f1c
sergey titar <titar_seryoga@mail.ru>
2023-01-08 20:19:33 +0700

    описание

---
+++ dz1_zadacha2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int a = -11;
+int b = -9;
+ 
+
+if (a>b) 
+{Console.Write("max=");
+Console.WriteLine(a);
+Console.Write("min=");
+Console.WriteLine(b);}
+    else
+{
+    
+Console.Write("max=");
+Console.WriteLine(b);
+Console.Write("min=");
+Console.WriteLine(a);
+}
