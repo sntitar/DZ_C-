155b079c24a9e8a06ca74804efff6bc99810a1e7
sergey titar <titar_seryoga@mail.ru>
2023-01-09 14:40:57 +0700

    код

---
+++ dz2_zadacha15/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Введите цифру, обозначающую день недели ");
+
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("да");
+    }
+
+    else Console.WriteLine("нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
