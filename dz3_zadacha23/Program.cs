fb7466342a33d11eb5b7027188facb4053cdeab8
sergey titar <titar_seryoga@mail.ru>
2023-01-11 18:38:10 +0700

    код

---
+++ dz3_zadacha23/Program.cs
@@ -0,0 +1,29 @@
+﻿void Cube(int[] cube)
+{
+    int counter = 0;
+    int length = cube.Length;
+    while (counter < length)
+    {
+        cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+        counter++;
+    }
+}
+
+void Print(int[] array)
+{
+    int count = array.Length;
+    int index = 0;
+    while (index < count)
+    {
+        Console.Write(array[index] + " ");
+        index++;
+    }
+}
+
+Console.Write("Введите число ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[cube + 1];
+Cube(array);
+Print(array);
+
