81eeb49a84e3152126d92fe7a5f3bf8ab8c13737
sergey titar <titar_seryoga@mail.ru>
2023-01-15 16:07:15 +0700

    код

---
+++ dz5_zadacha/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] CreateArray()
+{
+    int number = Random.Shared.Next(2, 10);
+    int[] array = new int[number];
+    return array;
+
+}
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(2, 10);
+    }
+}
+string PrintArray(int[] printArray)
+{
+    string output = string.Empty;
+    int size = printArray.Length;
+    for (int i = 0; i < size; i++)
+    {
+        output = output + $"{printArray[i]} ";
+    }
+    return output;
+}
+
+int[] newArray(int[] multi)
+{
+    int[] newArray = new int[multi.Length / 2];
+    for (int i = 0; i < multi.Length / 2; i++)
+    {
+        newArray[i] = multi[i] * multi[multi.Length - i - 1];
+    }
+    return newArray;
+}
+int[] array = CreateArray();
+FillArray(array);
+string Print = PrintArray(array);
+Console.WriteLine(Print);
+int[] newArr = newArray(array);
+string newPrint = PrintArray(newArr);
+Console.WriteLine(newPrint);
