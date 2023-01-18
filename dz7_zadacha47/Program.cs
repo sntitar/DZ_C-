bdd515bdd12df38930c87bad9de2ae569a0ba9ad
sergey titar <titar_seryoga@mail.ru>
2023-01-18 13:14:37 +0700

    описание

---
+++ dz7_zadacha47/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+double[,] array = new double[m, n];
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().NextDouble() * 10;
+        }
+    }
+}
+
+void WriteArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double alignNumber = Math.Round(array[i, j], 1);
+            Console.Write(alignNumber + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(array);
+WriteArray(array);
