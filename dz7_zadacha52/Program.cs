4c67b9fe605118a96de06cda2c0f70399173d36b
sergey titar <titar_seryoga@mail.ru>
2023-01-18 14:58:21 +0700

    код

--- dz7_zadacha52/Program.cs
+++ dz7_zadacha52/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿int m = InputNumbers("Введите количество строк m ");
+int n = InputNumbers("Введите количество столбцов n ");
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+PrintArray(matrix);
+for (int i = 0; i < n; i++)
+{
+    double arithmeticMean = 0;
+    for (int j = 0; j < m; j++)
+    {
+        arithmeticMean += matrix[j, i];
+    }
+    arithmeticMean = Math.Round(arithmeticMean / m, 1);
+    Console.WriteLine($"Cреднее арифметическое столбца № {i+1}: {arithmeticMean}");
+}
