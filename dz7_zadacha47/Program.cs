019fac32e3565053fc3325520c6527fbcde72bad
sergey titar <titar_seryoga@mail.ru>
2023-01-20 16:59:58 +0700

    код

--- dz7_zadacha47/Program.cs
+++ dz7_zadacha47/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.Write("Введите m ");
+﻿Console.Write("Введите количество строк m ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите n ");
+Console.Write("Введите количество столбцов n ");
 int n = Convert.ToInt32(Console.ReadLine());
 double[,] matrix = new double[m, n];
 void FillArray(double[,] matrix)
@@ -9,7 +9,7 @@ void FillArray(double[,] matrix)
     {
         for (int j = 0; j < n; j++)
         {
-            matrix[i, j] = new Random().NextDouble() * 10;
+            matrix[i, j] = new Random().NextDouble() * 20 - 10;
         }
     }
 }
@@ -25,6 +25,5 @@ void WriteArray(double[,] matrix)
         Console.WriteLine();
     }
 }
-
 FillArray(matrix);
 WriteArray(matrix);
--- dz7_zadacha50/Program.cs
+++ dz7_zadacha50/Program.cs
@@ -1,1 +1,90 @@
-﻿
+﻿int m = InputNumbers("Введите m ");
+int n = InputNumbers("Введите n ");
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+PrintArray(matrix);
+
+
+string? positionElement = Console.ReadLine();
+positionElement = RemovingSpaces(positionElement);
+int[] position = ParserString(positionElement);
+
+if(position[0] <= m 
+&& position[1] <= n 
+&& position[0] >= 0 
+&& position[1] >= 0) 
+{
+  double result = matrix[position[0]-1, position[1]-1];
+  Console.Write($"Значение элемента: {result}");
+}
+else Console.Write($"такого элемента в массиве нет.");
+
+int[] ParserString(string input)
+{
+  int countNumbers = 1;
+  for (int i = 0; i < input.Length; i++)
+  {
+      if (input[i] == ',')
+          countNumbers++;
+  }
+
+  int[] numbers = new int[countNumbers];
+
+  int numberIndex = 0;
+  for(int i = 0; i < input.Length; i++)
+  {
+    string subString = String.Empty;
+
+    while (input[i] != ',')
+    {
+      subString += input[i].ToString();
+      if (i >= input.Length - 1)
+        break;
+      i++;
+    }
+    numbers[numberIndex] = Convert.ToInt32(subString);
+    numberIndex++;
+  }
+
+  return numbers;
+}
+
+string RemovingSpaces (string input)
+{
+  string output = String.Empty;
+  for (int i = 0; i < input.Length; i++)
+  {
+    if (input[i] != ' ') 
+    {
+      output += input[i];
+    }
+  }
+  return output;
+}
