a92a71788d20274235aabc424f2a865e88553da2
sergey titar <titar_seryoga@mail.ru>
2023-01-20 20:02:45 +0700

    код

---
+++ dz9_zadacha64/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = InputNumbers("Введите натуральное число ");
+int m = 1;
+Console.WriteLine(number(n, m));
+int number(int n, int m)
+{
+    if (n == m) return n;
+    else
+        Console.Write($"{number(n, m + 1)}, ");
+    return m;
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
