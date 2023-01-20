2b745e0796f1aec7c9afe0a5f0e581db1f0337f3
sergey titar <titar_seryoga@mail.ru>
2023-01-20 20:11:17 +0700

    описание

---
+++ dz9_zadacha66/Program.cs
@@ -0,0 +1,15 @@
+﻿int m = InputNumbers("Введите m ");
+int n = InputNumbers("Введите n ");
+Console.WriteLine($"Сумма элементов: {sumNumber(m, n)}");
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+int sumNumber(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + sumNumber(m, n - 1);
+}
