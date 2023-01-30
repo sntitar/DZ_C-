dbf17076a6559cd20c21434d8a92f041deef1355
sergey titar <titar_seryoga@mail.ru>
2023-01-30 17:28:04 +0700

    готовый код

--- dz_itog/Program.cs
+++ dz_itog/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[5] { "Hello", "world", "2", "Denmark", "777" };
+string[] arrayNew = new string[array.Length];
+void SecondArray(string[] array, string[] arrayNew)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arrayNew[count] = array[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array, arrayNew);
+PrintArray(arrayNew);
